0d2c1b4b4974532609259b865ec2010f5d58cac3
Achraf Atauil <achrafatauil@hotmail.com>
2023-01-16 16:33:12 +0100

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
+
+module.exports = {
+  target: "web",
+  entry: {
+    index: './src/index.ts',
+  },
+  mode: isDev ? "development" : "production",
+  output: {
+    filename: 'index.js',
+    library: 'sparqlEditor',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: [/node_modules/, /test/],
+        use: [
+          {
+            loader: "swc-loader"
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')],
+    }),
+  ],
+};
