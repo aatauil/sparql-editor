d8f3b4f528e45ad122c7fb8812db0df7b832eb29
Achraf Atauil <achrafatauil@hotmail.com>
2023-01-24 10:33:05 +0100

    upgrade to codemirror 6

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,38 @@
-import "codemirror/lib/codemirror.css";
-import "codemirror/addon/fold/foldgutter.css";
-import "codemirror/addon/lint/lint.css"
-import "codemirror/addon/hint/show-hint.css"
-// import "./scss/codemirrorMods.scss"
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap, lineNumbers, highlightActiveLine, highlightActiveLineGutter } from "@codemirror/view";
+import { defaultKeymap, indentWithTab, history, historyKeymap } from "@codemirror/commands";
+import { StreamLanguage, defaultHighlightStyle, syntaxHighlighting, bracketMatching, foldGutter } from "@codemirror/language";
+import { highlightSelectionMatches } from "@codemirror/search";
+import {autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap} from "@codemirror/autocomplete"
 
-import "codemirror/addon/fold/foldcode.js";
-import "codemirror/addon/fold/brace-fold.js";
-import "codemirror/addon/fold/foldgutter.js";
-import "codemirror/addon/search/match-highlighter.js";
-import "codemirror/addon/edit/matchbrackets.js";
-import "codemirror/addon/lint/lint.js";
-import "codemirror/addon/hint/show-hint.js";
+import { sparql } from "@codemirror/legacy-modes/mode/sparql";
+import { linter, lintGutter } from "@codemirror/lint";
+import { sparqlLinter } from "./extentions/sparql-linter";
 
-import CodeMirror from "codemirror";
-import * as sparql11Mode from "./grammar/tokenizer";
-import { linter } from "./extentions/linter";
-import prefixFold from "./extentions/prefixFold";
+const extentions = [
+  keymap.of(defaultKeymap, indentWithTab), 
+  lineNumbers(),
+  StreamLanguage.define(sparql),
+  syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+  closeBrackets(),
+  linter(sparqlLinter),
+  lintGutter(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  bracketMatching(),
+  foldGutter(),
+]
 
-CodeMirror.registerHelper("fold", "prefix", prefixFold);
-CodeMirror.registerHelper("lint", "sparql11", linter);
-CodeMirror.defineMode("sparql11", sparql11Mode.default);
+export function view(element) {
 
-const defaultConfig = {
-    mode: "sparql11",
-    value: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  return new EditorView({
+    doc: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
 SELECT * WHERE {
   ?sub ?pred ?obj .
 } LIMIT 10`,
-    highlightSelectionMatches: true,
-    tabMode: "indent",
-    lineNumbers: true,
-    lineWrapping: true,
-    lint: {highlightLines: true},
-    foldGutter: {
-      rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.brace, CodeMirror.fold.prefix),
-    },
-    collapsePrefixesOnLoad: false,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
-    matchBrackets: true,
-    fixedGutter: true,
-    syntaxErrorCheck: true,
-    autocompleters: [] ,
+    parent: element,
+    extensions: extentions
+  })
 }
-
-export function SparqlEditor(elem, config) {
-  return new CodeMirror(elem, defaultConfig);
-}
-
