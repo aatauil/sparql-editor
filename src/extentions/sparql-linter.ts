e6b97e00f6d63bd9e0202ae66b8ee4bdb48874d5
Achraf Atauil <achrafatauil@hotmail.com>
2023-01-24 10:33:15 +0100

    update linter for codemirror 6

---
+++ src/extentions/sparql-linter.ts
@@ -0,0 +1,29 @@
+import CodeMirror from "codemirror";
+var SparqlParser = require('sparqljs').Parser;
+var parser = new SparqlParser();
+import { Diagnostic } from "@codemirror/lint";
+
+function pos(editor, { line, ch }) {
+  const doc = editor.state.doc;
+  return doc.line(line + 1).from + ch
+}
+
+export function sparqlLinter(editor, options) {
+  const diagnostics: Diagnostic[] = [];
+  const value = editor.state.doc.toString();
+
+  try {
+    const parsedQuery = parser.parse(value);
+  } catch (err) {
+    const hash = err.hash;
+
+    diagnostics.push({
+      from: pos(editor, { line: hash.loc.first_line - 1, ch: hash.loc.first_column }),
+      to: pos(editor, { line: hash.loc.last_line - 1, ch: hash.loc.last_column }),
+      message: err.message,
+      severity : "error"
+    })
+  }
+  
+  return diagnostics
+}
